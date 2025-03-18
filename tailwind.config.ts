0e5fac43bba1bc4c0df4fc4b058244c53e5cef72
Erfan sajjadi <40611694-eraerfanimia@users.noreply.replit.com>
2025-03-18 14:27:38 +0000

    Restored to '563618e3e7a56062ec632087b368dd0bdce053de'
    Replit-Restored-To: 563618e3e7a56062ec632087b368dd0bdce053de

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,9 +84,6 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-      boxShadow: {
-        'soft': '0 2px 15px -3px rgba(0, 0, 0, 0.07), 0 10px 20px -2px rgba(0, 0, 0, 0.04)',
-      },
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
